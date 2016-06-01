0887047869ba4591596e19a9b8a57c0d8f175183
Marc Byrd <dr.marc.byrd@gmail.com>
2016-06-01 15:04:45 -0700

    Less verbose

--- connect4.rb
+++ connect4.rb
@@ -3,10 +3,6 @@ require 'logger'
 """
 The task is to write an implementation of the game Connect-4 (http://en.wikipedia.org/wiki/Connect_Four).
 
-In Connect-4, there are two players, and each of the players take turns trying to get four of their game pieces in a column.
-
-Write a functional subset of the code for the game board, game pieces, and player state. Your code should include functions to place a piece into the game board and check if one of the players has won the game. Your code should simulate the turns taken by players (in other words, you do NOT have to rowlect actual input from players, so you should not build UI to do so).
-
 Your code should also include a function to print out the board, in ASCII. For example, here is a plausible print out after two turns - one from player 'x' and player 'o':
 
 * * * * * * *
